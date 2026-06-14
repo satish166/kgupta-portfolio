b7083bdd8c4af13f13fbfc745d8890022425a607
Satish Sandhu <sandhusatish166@gmail.com>
2026-06-14 06:16:15 -0700

    chore: generate build artifacts and cache files in .next directory

--- .next/server/app/api/portfolio/route.js
+++
@@ -1,7 +0,0 @@
-var R=require("../../../chunks/[turbopack]_runtime.js")("server/app/api/portfolio/route.js")
-R.c("server/chunks/[root-of-the-server]__10ced160._.js")
-R.c("server/chunks/[root-of-the-server]__b2da729a._.js")
-R.c("server/chunks/node_modules_next_dist_esm_build_templates_app-route_d3534735.js")
-R.c("server/chunks/_next-internal_server_app_api_portfolio_route_actions_9cd68082.js")
-R.m(18808)
-module.exports=R.m(18808).exports
--- .next/server/server-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"HtfTLo4lwts2gGNWAwpA+3onu4MJg+l3wflS8B0vfK4=\"\n}"
